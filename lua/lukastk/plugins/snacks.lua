5f7a4a26a862a87be6c5883b3e863a1e2c96bbe0
EfrenDaMo <david.mora.work@gmail.com>
2025-08-17 19:20:22 -0600

    Changed some snack stuff that is annoying me

--- lua/lukastk/plugins/snacks.lua
+++ lua/lukastk/plugins/snacks.lua
@@ -1,20 +1,20 @@
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	priority = 1000,
-	event = "UIEnter",
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    event = "UIEnter",
 
-	---@type snacks.Config
-	opts = {
-		image = { enabled = true },
-		input = { enabled = true },
-		words = { enabled = true },
-		scope = { enabled = true },
-		toggle = { enabled = true },
-		bigfile = { enabled = true },
-		statuscolumn = { enabled = true },
-		git = { enabled = true },
-		lazygit = { enabled = true },
-		gitbrowse = { enabled = true },
-	},
+    ---@type snacks.Config
+    opts = {
+        image = { enabled = false },
+        input = { enabled = true },
+        words = { enabled = true },
+        scope = { enabled = true },
+        toggle = { enabled = true },
+        bigfile = { enabled = true },
+        statuscolumn = { enabled = true },
+        git = { enabled = true },
+        lazygit = { enabled = true },
+        gitbrowse = { enabled = true },
+    },
 }
